5ce6c01be8f5a8db1daa0537f517efe4b4c2b4ad
AdamRFaqih <adamrafif@student.telkomuniversity.ac.id>
2023-07-22 16:11:23 +0700

    add database connection

--- server.js
+++ server.js
@@ -1,41 +1,50 @@
 const express = require('express');
-const app = express();
-const port = 8080; // You can change the port number if needed
+const mysql = require('mysql');
 
-// Sample student ID and acceptance status data (replace this with your actual data or integrate with a database)
-const studentData = {
-    "12345": { accepted: true, password: "password123" },
-    "67890": { accepted: false, password: "mypassword" },
-    // Add more student ID-password pairs as needed
-};
+const app = express();
+const port = 8080;
+
+// Create a MySQL connection pool
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost', // Change this to your MySQL host (e.g., localhost)
+  user: 'root', // Change this to your MySQL username
+  password: '', // Change this to your MySQL password
+  database: 'students_db', // Change this to your database name
+});
 
 // Endpoint to check acceptance status
 app.get('/check-status', (req, res) => {
-    const studentId = req.query.studentId;
-    const student = studentData[studentId];
+  const studentId = req.query.studentId;
+  const password = req.query.password;
 
-    if (!student) {
-        res.json({ message: "Invalid Student ID. Please check and try again." });
+  // Query the database for the student ID and unique code
+  const sql = 'SELECT * FROM students WHERE student_id = ? AND password = ?';
+  db.query(sql, [studentId, password], (err, results) => {
+    if (err) {
+      console.error('Database query error:', err.message);
+      res.json({ success: false, message: 'An error occurred while checking the status. Please try again.' });
     } else {
-        const enteredPassword = req.query.password;
-        if (enteredPassword === student.password) {
-            if (student.accepted) {
-                res.json({ message: "Congratulations! You have been accepted." });
-            } else {
-                res.json({ message: "We regret to inform you that you have been rejected." });
-            }
+      if (results.length > 0) {
+        const student = results[0];
+        if (student.accepted) {
+          res.json({ success: true, message: 'Congratulations! You have been accepted.' });
         } else {
-            res.json({ message: "Incorrect password. Please try again." });
+          res.json({ success: false, message: 'We regret to inform you that you have been rejected.' });
         }
+      } else {
+        res.json({ success: false, message: 'Invalid Student ID or incorrect unique code. Please try again.' });
+      }
     }
+  });
 });
 
 // Default route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
